6d9ab18aa82d54e6cf8993903955fa1b4bbd10ba
Abhishek Gupta <ag528927@gmail.com>
2021-08-15 13:33:55 +0000

    Fixes

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 var minutes = 1
 var seconds = 30
+var flag = false
+var interval;
 
 let btn = document.getElementById("btn")
 
-function startTimer(){
-  setInterval(() => {
-    if(minutes === 0 && seconds === 1){
-      document.getElementById("timer").innerHTML = "00:00"
-    }else{
-      seconds = seconds - 1
-      if(seconds === 0){
-        minutes = minutes - 1
-        seconds = 60
-      }
+function start(){
+  flag = true
+  TimerStart()
+}
 
-      let minString = minutes
-      let secString = seconds
-      if(minutes.toString().length == 1){
-        minString = "0"+minutes
-      }
-      if(seconds.toString().length == 1){
-        secString = "0"+seconds
-      }
+function resetTimer(){
+  flag = false
+  clearInterval(interval)
+  minutes = 1
+  seconds = 30
+  document.getElementById("timer").innerHTML = "00:00"
+}
 
-      document.getElementById("timer").innerHTML = `${minString}:${secString}`
-    }
-  }, 1000)
+function TimerStart(){
+  if(flag === true){
+    interval = setInterval(() => {
+      if(minutes === 0 && seconds === 1){
+        document.getElementById("timer").innerHTML = "00:00"
+      }else{
+        seconds = seconds - 1
+        if(seconds === 0){
+          minutes = minutes - 1
+          seconds = 60
+        }
+
+        let minString = minutes
+        let secString = seconds
+        if(minutes.toString().length == 1){
+          minString = "0"+minutes
+        }
+        if(seconds.toString().length == 1){
+          secString = "0"+seconds
+        }
+
+        document.getElementById("timer").innerHTML = `${minString}:${secString}`
+      }
+    }, 1000)
+  }
 }
 
